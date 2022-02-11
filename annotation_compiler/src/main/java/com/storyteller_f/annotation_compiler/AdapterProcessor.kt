4460a7fee668c75cd02799123be59b90bdedfbfc
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-02-11 23:35:27 +0800

    first commit

---
+++ annotation_compiler/src/main/java/com/storyteller_f/annotation_compiler/AdapterProcessor.kt
@@ -0,0 +1,11 @@
+package com.storyteller_f.annotation_compiler
+
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.RoundEnvironment
+import javax.lang.model.element.TypeElement
+
+class AdapterProcessor : AbstractProcessor() {
+    override fun process(p0: MutableSet<out TypeElement>?, p1: RoundEnvironment?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
