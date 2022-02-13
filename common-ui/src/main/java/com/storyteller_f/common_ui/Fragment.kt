b22657c995b38e8b5b96226c39b4dc5c090883f6
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-02-13 19:23:55 +0800

    common fragment

---
+++ common-ui/src/main/java/com/storyteller_f/common_ui/Fragment.kt
@@ -0,0 +1,25 @@
+package com.storyteller_f.common_ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * @author storyteller_f
+ */
+
+abstract class CommonFragment(private val layoutId: Int) : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val inflate = inflater.inflate(layoutId, container)!!
+        onBindViewEvent(inflate)
+        return inflate
+    }
+
+    abstract fun onBindViewEvent(inflate: View)
+}
