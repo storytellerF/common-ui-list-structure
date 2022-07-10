90a1c2fc73cfae1c9d54b2825900e7cbf9172ba5
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-07-10 22:32:54 +0800

    [app:giant-explorer] request root

---
+++ app/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/control/RootAccessActivity.kt
@@ -0,0 +1,50 @@
+package com.storyteller_f.giant_explorer.control
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.storyteller_f.common_ktx.exceptionMessage
+import com.storyteller_f.giant_explorer.R
+import com.storyteller_f.giant_explorer.databinding.ActivityRootAccessBinding
+
+class RootAccessActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityRootAccessBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRootAccessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_root_access)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            try {
+                val exec = Runtime.getRuntime().exec("su --version")
+                val waitFor = exec.waitFor()
+                Toast.makeText(this, "wait:$waitFor", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Toast.makeText(this, e.exceptionMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_root_access)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
