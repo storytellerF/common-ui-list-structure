e157a561999b1cb950408c03ed1a7499214a5a97
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-07-21 12:24:24 +0800

    update gradle

--- common-ui/src/main/java/com/storyteller_f/common_ui/InsetBlock.java
+++
@@ -1,7 +0,0 @@
-package com.storyteller_f.common_ui;
-
-import androidx.annotation.IntDef;
-
-@IntDef(flag = true, value = {InsetBlockDirection.top, InsetBlockDirection.start, InsetBlockDirection.end, InsetBlockDirection.bottom})
-@interface InsetBlockFlag {
-}
