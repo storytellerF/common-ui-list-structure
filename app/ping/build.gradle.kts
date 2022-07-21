1b21826b61005d2c5b6efa8312dae616b4848b94
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-07-21 16:04:29 +0800

    使用kotlin dsl 处理sml，可以带有代码自动完成

---
+++ app/ping/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.storyteller_f.sml")
+}
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.storyteller_f.ping"
+        minSdkPreview = "21"
+        targetSdkPreview = "32"
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.0")
+    implementation("com.google.android.exoplayer:exoplayer-core:2.18.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+sml {
+    color.set(mutableMapOf("test" to "#ff0000"))
+    dimen.set(mutableMapOf("test1" to "12"))
+    shapes {
+        register("hello") {
+            solidColor.set("#00ff00")
+            radius.set(12)
+        }
+    }
+}
