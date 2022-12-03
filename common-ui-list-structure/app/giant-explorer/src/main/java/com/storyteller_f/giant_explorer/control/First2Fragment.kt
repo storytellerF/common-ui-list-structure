c4409a85159a295accd7c2f39c50f35a7c918c19
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-12-03 19:34:02 +0800

    code impr

--- common-ui-list-structure/app/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/control/First2Fragment.kt
+++ common-ui-list-structure/app/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/control/First2Fragment.kt
@@ -1,45 +1,19 @@
 package com.storyteller_f.giant_explorer.control
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.storyteller_f.common_ui.SimpleFragment
 import com.storyteller_f.giant_explorer.R
 import com.storyteller_f.giant_explorer.databinding.FragmentFirst2Binding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class First2Fragment : Fragment() {
+class First2Fragment : SimpleFragment<FragmentFirst2Binding>(FragmentFirst2Binding::inflate) {
 
-    private var _binding: FragmentFirst2Binding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirst2Binding.inflate(inflater, container, false)
-        return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun onBindViewEvent(binding: FragmentFirst2Binding) {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_First2Fragment_to_Second2Fragment)
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
--- common-ui-list-structure/app/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/control/Second2Fragment.kt
+++ common-ui-list-structure/app/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/control/Second2Fragment.kt
@@ -1,45 +1,20 @@
 package com.storyteller_f.giant_explorer.control
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.storyteller_f.common_ui.SimpleFragment
 import com.storyteller_f.giant_explorer.R
 import com.storyteller_f.giant_explorer.databinding.FragmentSecond2Binding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class Second2Fragment : Fragment() {
+class Second2Fragment : SimpleFragment<FragmentSecond2Binding>(FragmentSecond2Binding::inflate) {
 
-    private var _binding: FragmentSecond2Binding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentSecond2Binding.inflate(inflater, container, false)
-        return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun onBindViewEvent(binding: FragmentSecond2Binding) {
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_Second2Fragment_to_First2Fragment)
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
