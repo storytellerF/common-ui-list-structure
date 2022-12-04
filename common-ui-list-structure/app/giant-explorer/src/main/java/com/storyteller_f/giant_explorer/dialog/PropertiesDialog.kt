a0753b11d64243dcc9d9c6f983d7c2d0fd68774d
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-12-04 13:22:34 +0800

    [giant-explorer]显示mp4 详情

--- common-ui-list-structure/app/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/dialog/PropertiesDialog.kt
+++ common-ui-list-structure/app/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/dialog/PropertiesDialog.kt
@@ -3,6 +3,8 @@ package com.storyteller_f.giant_explorer.dialog
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.media.MediaExtractor
+import android.media.MediaFormat
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -13,6 +15,7 @@ import com.storyteller_f.common_ui.setOnClick
 import com.storyteller_f.giant_explorer.control.getFileInstance
 import com.storyteller_f.giant_explorer.databinding.DialogFilePropertiesBinding
 import com.storyteller_f.giant_explorer.model.FileModel
+import kotlin.time.Duration
 
 class PropertiesDialog : SimpleDialogFragment<DialogFilePropertiesBinding>(DialogFilePropertiesBinding::inflate) {
     private val args by navArgs<PropertiesDialogArgs>()
@@ -29,10 +32,43 @@ class PropertiesDialog : SimpleDialogFragment<DialogFilePropertiesBinding>(Dialo
         val length = if (fileInstance.isFile) fileInstance.fileLength
         else 0
         binding.model = FileModel(fileInstance.name, fileInstance.path, length, fileInstance.isHidden, fileInstance.fileSystemItem)
+        if (fileInstance.isFile) {
+            if (fileInstance.file.extension == "mp4") {
+                val mediaExtractor = MediaExtractor()
+                mediaExtractor.setDataSource(fileInstance.path)
+                val filter = totalSequence(mediaExtractor.trackCount) {
+                    mediaExtractor.getTrackFormat(it)
+                }.filter {
+                    it.getString(MediaFormat.KEY_MIME)?.startsWith("video") == true
+                }.first()
+                val width = filter.getInteger(MediaFormat.KEY_WIDTH)
+                val height = filter.getInteger(MediaFormat.KEY_HEIGHT)
+                val duration = filter.getLong(MediaFormat.KEY_DURATION)
+                binding.videoInfo.text = """
+                    track count ${mediaExtractor.trackCount}
+                    width $width
+                    height $height
+                    duration $duration ms
+                """.trimIndent()
+
+                mediaExtractor.release()
+            }
+        }
+
     }
 }
 
 fun Context.copyText(text: CharSequence) {
     ContextCompat.getSystemService(this, ClipboardManager::class.java)?.setPrimaryClip(ClipData.newPlainText("text", text))
     Toast.makeText(this, "copied", Toast.LENGTH_SHORT).show()
+}
+
+fun <T : Any> totalSequence(total: Int, block: (Int) -> T): Sequence<T> {
+    var start = 0
+    return sequence {
+        if (start < total) {
+            yield(block(start))
+            start++
+        }
+    }
 }
