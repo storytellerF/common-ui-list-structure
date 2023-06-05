5e4dbf969f399d73ab3d16510187b46023296d84
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-06-05 08:30:32 +0800

    Ping

---
+++ examples/Ping/build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    dependencies {
+        val smlFolder: String? by project
+        val smlVersion = "0.0.2"
+        val navVersion = "2.5.3"
+
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        when (smlFolder) {
+            "remote", null -> classpath("com.github.storytellerF.SML:com.storyteller_f.sml.gradle.plugin:$smlVersion")
+            "repository" -> classpath("com.storyteller_f.sml:sml:$smlVersion")
+        }
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    val androidVersion = "8.1.0-beta04"
+    val kotlinVersion = "1.8.21"
+    val kspVersion = "1.8.21-1.0.11"
+    id("com.android.application") version androidVersion apply false
+    id("com.android.library") version androidVersion apply false
+    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion apply false
+    id("com.google.devtools.ksp") version kspVersion apply false
+}
---
+++ examples/Ping/settings.gradle.kts
@@ -0,0 +1,53 @@
+pluginManagement {
+    includeBuild("../../common-ui-list-structure/version-manager")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            setUrl("https://jitpack.io")
+        }
+    }
+}
+//todo 生成脚本，自动完成此工作
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://artifactory.cronapp.io/public-release/") }
+    }
+}
+
+rootProject.name = "Ping"
+include(":ping")
+
+val commonUiListModules = listOf(
+    "common-ktx",
+    "common-pr",
+    "common-ui",
+    "common-vm-ktx",
+    "compat-ktx",
+    "composite-compiler",
+    "composite-definition",
+    "config",
+    "ext-func-compiler",
+    "ext-func-definition",
+    "file-system",
+    "file-system-ktx",
+    "multi-core",
+    "slim-ktx",
+    "ui-list",
+    "ui-list-annotation-common",
+    "ui-list-annotation-compiler",
+    "ui-list-annotation-compiler-ksp",
+    "ui-list-annotation-definition",
+    "view-holder-compose"
+)
+val commonUiPath = File(rootDir, "../../common-ui-list-structure")
+commonUiListModules.forEach {
+    val modulePath = File(commonUiPath, it)
+    include(it)
+    project(":$it").projectDir = modulePath
+}
