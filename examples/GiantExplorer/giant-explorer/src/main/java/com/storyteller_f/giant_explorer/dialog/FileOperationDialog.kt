0b79e72b4a549de288c5e3f17c30f41ea15f657e
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-06-30 21:16:06 +0800

    code impr

--- examples/GiantExplorer/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/dialog/FileOperationDialog.kt
+++ examples/GiantExplorer/giant-explorer/src/main/java/com/storyteller_f/giant_explorer/dialog/FileOperationDialog.kt
@@ -8,8 +8,17 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.repeatOnLifecycle
 import com.storyteller_f.common_pr.state
-import com.storyteller_f.common_ui.*
-import com.storyteller_f.common_vm_ktx.*
+import com.storyteller_f.common_ui.SimpleDialogFragment
+import com.storyteller_f.common_ui.onVisible
+import com.storyteller_f.common_ui.owner
+import com.storyteller_f.common_ui.pp
+import com.storyteller_f.common_ui.scope
+import com.storyteller_f.common_vm_ktx.GenericValueModel
+import com.storyteller_f.common_vm_ktx.avm
+import com.storyteller_f.common_vm_ktx.debounce
+import com.storyteller_f.common_vm_ktx.keyPrefix
+import com.storyteller_f.common_vm_ktx.to
+import com.storyteller_f.common_vm_ktx.vm
 import com.storyteller_f.file_system.operate.DefaultForemanProgressListener
 import com.storyteller_f.giant_explorer.R
 import com.storyteller_f.giant_explorer.databinding.DialogFileOperationBinding
@@ -17,18 +26,15 @@ import com.storyteller_f.giant_explorer.service.FileOperateBinder
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
-import java.util.*
+import java.util.UUID
 
-class FileOperationDialog : SimpleDialogFragment<DialogFileOperationBinding>(DialogFileOperationBinding::inflate) {
+class FileOperationDialog :
+    SimpleDialogFragment<DialogFileOperationBinding>(DialogFileOperationBinding::inflate) {
     lateinit var binder: FileOperateBinder
 
-    //    private val progressVM by kvm("progress", {}) {
-//        GenericValueModel<Int>()
-//    }
-    private val progressVM by keyPrefix({ "progresss" }, vm({}) {
+    private val progressVM by keyPrefix({ "progress" }, vm({}) {
         GenericValueModel<Int>()
-    }
-    )
+    })
     private val leftVM by keyPrefix("left", vm({}) {
         GenericValueModel<Triple<Int, Int, Long>>().apply {
             data.value = -1 to -1 to -1
@@ -73,13 +79,15 @@ class FileOperationDialog : SimpleDialogFragment<DialogFileOperationBinding>(Dia
     private fun bindListener(key: String, binding: DialogFileOperationBinding) {
         val orPut = binder.fileOperationProgressListener.getOrPut(key) { mutableListOf() }
         orPut.add(object : DefaultForemanProgressListener() {
-            override fun onProgress(progress: Int, key: String) = progressVM.data.postValue(progress)
+            override fun onProgress(progress: Int, key: String) =
+                progressVM.data.postValue(progress)
 
             override fun onState(state: String?, key: String) = stateVM.data.postValue(state)
 
             override fun onTip(tip: String?, key: String) = tipVM.data.postValue(tip)
 
-            override fun onLeft(fileCount: Int, folderCount: Int, size: Long, key: String) = leftVM.data.postValue(fileCount to folderCount to size)
+            override fun onLeft(fileCount: Int, folderCount: Int, size: Long, key: String) =
+                leftVM.data.postValue(fileCount to folderCount to size)
 
             override fun onComplete(dest: String?, isSuccess: Boolean, key: String) {
                 binding.closeWhenError.pp {
@@ -114,17 +122,25 @@ class FileOperationDialog : SimpleDialogFragment<DialogFileOperationBinding>(Dia
                     val task = binder.map[key]?.taskAssessResult
                     list.onVisible(binding.stateRunning)
                     Log.i(TAG, "onBindViewEvent: $key $task ${binder.map.keys}")
-                    binding.textViewTask.text = String.format("total size: %d\ntotal file:%d\ntotal folder:%d", task?.size, task?.fileCount, task?.folderCount)
+                    binding.textViewTask.text = String.format(
+                        "total size: %d\ntotal file:%d\ntotal folder:%d",
+                        task?.size,
+                        task?.fileCount,
+                        task?.folderCount
+                    )
                 }
+
                 FileOperateBinder.state_end -> {
                     binding.doneText.text = getString(R.string.operation_task_done)
                     list.onVisible(binding.stateDone)
                 }
+
                 FileOperateBinder.state_error -> {
                     val task = binder.map[key]
                     binding.doneText.text = task?.message
                     list.onVisible(binding.stateDone)
                 }
+
                 else -> list.onVisible(binding.stateProgress)
             }
         }
@@ -143,7 +159,8 @@ class FileOperationDialog : SimpleDialogFragment<DialogFileOperationBinding>(Dia
         }
     }
 
-    private fun presentTaskSnapshot(it: Triple<Int, Int, Long>) = String.format("size: %d\nleft file:%d\nleft folder:%d", it.third, it.first, it.second)
+    private fun presentTaskSnapshot(it: Triple<Int, Int, Long>) =
+        String.format("size: %d\nleft file:%d\nleft folder:%d", it.third, it.first, it.second)
 
     override fun onDestroyView() {
         super.onDestroyView()
