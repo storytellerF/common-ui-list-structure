0b088479098ad02b083f6396f061f2988f3f817d
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-08-07 21:04:50 +0800

    rm core.aar

--- examples/GiantExplorer/giant-explorer-plugin-core/build.gradle.kts
+++ examples/GiantExplorer/giant-explorer-plugin-core/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.gradle.internal.tasks.factory.dependsOn
-import org.jetbrains.kotlin.util.capitalizeDecapitalize.toUpperCaseAsciiOnly
 import com.storyteller_f.version_manager.*
 
 plugins {
@@ -26,28 +24,28 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-val target = listOf("yue", "li")
-val dispatchTasks = target.map { targetName ->
-    tasks.register("dispatchPluginCoreLibTo${targetName.toUpperCaseAsciiOnly()}", Copy::class) {
-        val aarFileName = "giant-explorer-plugin-core-debug.aar"
-        val aarFile = File(buildDir, "/outputs/aar/$aarFileName")
-        val dest = File(rootDir, "../../giant-explorer/$targetName/giant-explorer-plugin-core")
-
-        from(aarFile) {
-            rename {
-                it.replace(aarFileName, "core.aar")
-            }
-        }
-        into(dest)
-        println("dest : ${dest.absolutePath} aar: $aarFile")
-    }
-}
-
-afterEvaluate {
-    val taskName = "bundleDebugAar"
-    val packageDebug = tasks.findByName(taskName)
-    dispatchTasks.forEach {
-        it.dependsOn(taskName)
-        packageDebug?.finalizedBy(it)
-    }
-}
+//val target = listOf("yue", "li")
+//val dispatchTasks = target.map { targetName ->
+//    tasks.register("dispatchPluginCoreLibTo${targetName.toUpperCaseAsciiOnly()}", Copy::class) {
+//        val aarFileName = "giant-explorer-plugin-core-debug.aar"
+//        val aarFile = File(buildDir, "/outputs/aar/$aarFileName")
+//        val dest = File(rootDir, "../../giant-explorer/$targetName/giant-explorer-plugin-core")
+//
+//        from(aarFile) {
+//            rename {
+//                it.replace(aarFileName, "core.aar")
+//            }
+//        }
+//        into(dest)
+//        println("dest : ${dest.absolutePath} aar: $aarFile")
+//    }
+//}
+//
+//afterEvaluate {
+//    val taskName = "bundleDebugAar"
+//    val packageDebug = tasks.findByName(taskName)
+//    dispatchTasks.forEach {
+//        it.dependsOn(taskName)
+//        packageDebug?.finalizedBy(it)
+//    }
+//}
